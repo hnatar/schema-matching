f0915c4af60201409df9bed79cdb6eab578f42ba
hnataraj@usc.edu <hnataraj@usc.edu>
2018-09-10 16:42:42 -0700

    init

---
+++ test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+Schema Matching
+
+0.1.0		- Proof of concept for simple instance matching based on values ranges (for numeric),
+			  predicted category / type of data (string, int, option-type, etc.).
+			  Membership in category is a simple feature, features are fed to a classifier which
+			  can be decision tree or logistic multiclass.
+
+			  Value range modelling based on GAN approach ?
+"""
