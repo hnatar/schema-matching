22161047eb81594fe2fd689f5c62775e020cd889
hnataraj@usc.edu <hnataraj@usc.edu>
2018-11-21 12:46:07 -0800

    change to generate addresses

--- markov_generate.py
+++ markov_generate.py
@@ -6,7 +6,7 @@ Markov text generator, to create 'random' samples.
 import markovify
 
 # Get raw text as string.
-with open("firstnames.txt") as f:
+with open("LA_Addresses.txt") as f:
     text = f.read()
 print(text)
 
@@ -15,7 +15,7 @@ text_model = markovify.NewlineText(text)
 
 # Print five randomly-generated sentences
 for i in range(5):
-    print(text_model.make_sentence(tries=10000))
+    print(text_model.make_sentence())
 
 # Print three randomly-generated sentences of no more than 140 characters
 for i in range(3):
